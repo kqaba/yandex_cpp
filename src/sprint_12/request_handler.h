644d674bb76a9f83c3ee2362e93234851c535014
kqaba <110821533+kqaba@users.noreply.github.com>
2023-04-29 00:01:19 +0300

    transport_catalogue

---
+++ src/sprint_12/request_handler.h
@@ -0,0 +1,33 @@
+#pragma once 
+#include "transport_catalogue.h" 
+#include "map_renderer.h" 
+#include "json.h" 
+#include <optional> 
+ 
+struct Requests { 
+    std::deque<json::Node> stop_requests; 
+    std::deque<json::Node> bus_requests; 
+}; 
+ 
+ 
+namespace request_handler { 
+    class RequestHandler { 
+    public: 
+        RequestHandler(const transport_catalogue::TransportCatalogue& transport_catalogue, const MapRenderer& map_render); 
+ 
+        std::optional<transport_catalogue::BusInfo> GetBusInfo(const std::string_view bus_name) const; 
+        std::optional<transport_catalogue::StopInfo> GetStopInfo(const std::string_view stop_name) const; 
+ 
+        std::vector<geo::Coordinates> GetCoordinates() const; 
+ 
+        const transport_catalogue::Bus* GetBus(const std::string bus_name) const; 
+        transport_catalogue::Stop GetStop(const std::string_view stop_name) const; 
+ 
+ 
+        void RenderMap(std::ostream& output) const; 
+ 
+    private: 
+        const transport_catalogue::TransportCatalogue& db_; 
+        const MapRenderer& renderer_; 
+    }; 
+} 
