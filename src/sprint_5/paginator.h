a3c23ca98150d7be38b97353fcfd3c54cde4d169
kqaba <110821533+kqaba@users.noreply.github.com>
2023-04-28 23:07:42 +0300

    SearchServer

---
+++ src/sprint_5/paginator.h
@@ -0,0 +1,60 @@
+#pragma once 
+#include <algorithm> 
+#include <iostream> 
+#include <vector> 
+    template <class Iterator> 
+    struct IteratorsRange { 
+        Iterator begin_; 
+        Iterator end; 
+        explicit IteratorsRange(Iterator range_start, Iterator range_end) : begin_(range_start), end(range_end) {} 
+    }; 
+ 
+    template <class Iterator> 
+    std::ostream& operator<<(std::ostream& os, const IteratorsRange<Iterator>& range) { 
+        for (auto iter = range.begin_; iter != range.end; ++iter) 
+            os << *iter; 
+        return os; 
+    } 
+ 
+    template <class Iterator> 
+    class Paginator { 
+    public:   
+        Paginator() = default; 
+ 
+    public:   
+        void Init(Iterator begin, Iterator end, int page_size) { 
+            int elements_left{ static_cast<int>(std::distance(begin, end)) }; 
+            int elements_fit_page{ 0 }; 
+ 
+            while (elements_left > 0) { 
+                elements_fit_page = std::min(page_size, elements_left); 
+                pages_.emplace_back(begin, begin + elements_fit_page); 
+ 
+                begin += elements_fit_page; 
+                elements_left -= elements_fit_page; 
+            } 
+        } 
+ 
+        auto begin() const { 
+            return pages_.begin(); 
+        } 
+ 
+        auto end() const { 
+            return pages_.end(); 
+        } 
+ 
+        size_t size() const { 
+            return pages_.size(); 
+        } 
+ 
+    private:  
+        std::vector<IteratorsRange<Iterator>> pages_; 
+    }; 
+ 
+    template <typename Container> 
+    auto Paginate(const Container& container, int page_size) { 
+        Paginator<decltype(container.begin())> paginator; 
+        paginator.Init(container.begin(), container.end(), page_size); 
+ 
+        return paginator; 
+    } 
