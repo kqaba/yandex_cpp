4b67d9a844dc8bea16fac83dcf0b986dec7366cf
kqaba <110821533+kqaba@users.noreply.github.com>
2023-04-28 23:11:42 +0300

    SearchServer

---
+++ src/sprint_5/search_server.cpp
@@ -0,0 +1,96 @@
+#include "search_server.h"  
+#include "document.h"  
+  
+#include <cmath>  
+#include <iostream>  
+#include <numeric>  
+#include <stdexcept>  
+#include <string>   
+#include <bitset>  
+ 
+void SearchServer::AddDocument(int document_id, const std::string& document, DocumentStatus status,  
+    const std::vector<int>& ratings) {  
+   using namespace std::literals; 
+    if ((document_id < 0) || (documents_.count(document_id) > 0)) {  
+        throw std::invalid_argument("Неверный документ айди"s);  
+    }  
+    const auto words = SplitIntoWordsNoStop(document);  
+    const double inv_word_count = 1.0 / words.size();  
+    for (const std::string& word : words) {  
+        word_to_document_freqs_[word][document_id] += inv_word_count;  
+    }  
+    documents_.emplace(document_id, DocumentData{ ComputeAverageRating(ratings), status });  
+    document_ids_.push_back(document_id);  
+}  
+  
+ std::vector<std::string> SearchServer::SplitIntoWordsNoStop(const std::string& text) const{  
+     std::vector<std::string> words;  
+     using namespace std::literals; 
+        for (const std::string& word : SplitIntoWords(text)) {  
+            if (!IsValidWord(word)) {  
+                throw std::invalid_argument("слово "s + word + " недопустимо"s);  
+            }  
+            if (!IsStopWord(word)) {  
+                words.push_back(word);  
+            }  
+        }  
+        return words;  
+    }   
+ 
+ SearchServer::Query SearchServer::ParseQuery(const std::string& text) const {  
+        Query result;  
+        for (const std::string& word : SplitIntoWords(text)) {  
+            const auto query_word = ParseQueryWord(word);  
+            if (!query_word.is_stop) {  
+                if (query_word.is_minus) {  
+                    result.minus_words.insert(query_word.data);  
+                }  
+                else {  
+                    result.plus_words.insert(query_word.data);  
+                }  
+            }  
+        }  
+        return result;  
+    }  
+ 
+std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_query, DocumentStatus status) const {  
+    return FindTopDocuments(  
+        raw_query, [status](int document_id, DocumentStatus document_status, int rating) {  
+            return document_status == status;  
+        });  
+}  
+  
+std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_query) const {  
+    return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);  
+}  
+  
+int SearchServer::GetDocumentCount() const {  
+    return documents_.size();  
+}  
+  
+int SearchServer::GetDocumentId(int index) const {  
+    return document_ids_.at(index);  
+}  
+std::tuple<std::vector<std::string>, DocumentStatus> SearchServer::MatchDocument(const std::string& raw_query,  
+    int document_id) const {  
+    const auto query = ParseQuery(raw_query);  
+    std::vector<std::string> matched_words;  
+    for (const std::string& word : query.plus_words) {  
+        if (word_to_document_freqs_.count(word) == 0) {  
+            continue;  
+        }  
+        if (word_to_document_freqs_.at(word).count(document_id)) {  
+            matched_words.push_back(word);  
+        }  
+    }  
+    for (const std::string& word : query.minus_words) {  
+        if (word_to_document_freqs_.count(word) == 0) {  
+            continue;  
+        }  
+        if (word_to_document_freqs_.at(word).count(document_id)) {  
+            matched_words.clear();  
+            break;  
+        }  
+    }  
+    return { matched_words, documents_.at(document_id).status };  
+}  
